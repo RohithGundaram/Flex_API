a276db403af1af08af16b11b13f5579bddb723a7
Rohith Gundaram <87968757+RohithGundaram@users.noreply.github.com>
2024-04-25 12:02:12 +0530

    Update Users.js

--- Models/Users.js
+++ Models/Users.js
@@ -3,6 +3,7 @@ import mongoose,{Schema} from "mongoose";
 const userSchema=new Schema(
     {
         email:String,
+        password:String,
         phone:String,
         MacAddress:String,
         emergencyContact:String,   //email
