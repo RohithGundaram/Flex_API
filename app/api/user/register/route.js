7fcbb9b93f676fa9fdfcdfa6d265000236bd66b5
Rohith Gundaram <87968757+RohithGundaram@users.noreply.github.com>
2024-04-26 20:25:45 +0530

    Update route.js

--- app/api/user/register/route.js
+++ app/api/user/register/route.js
@@ -4,11 +4,11 @@ import { NextResponse } from "next/server"
 
 
 export async function POST(request){
-    const {email,password,phone,MacAddress,emergencyContact,deviceToken}=await request.json()
+    const {email,password,phone,MacAddress,emergencyContact}=await request.json()
     await connectMongo()
 
     //write a logic to check whether the user already exist in Database
-    await Users.create({email,password,phone,MacAddress,emergencyContact,deviceToken})
+    await Users.create({email,password,phone,MacAddress,emergencyContact})
     return NextResponse.json({message:"User registered"},{status:201})
 }
 
