3456519df02230e94afafb6ef892cc10efa2c816
Rohith Gundaram <87968757+RohithGundaram@users.noreply.github.com>
2024-04-26 23:02:37 +0530

    Update route.js

--- app/api/accidents/route.js
+++ app/api/accidents/route.js
@@ -35,7 +35,7 @@ export async function POST(request){
 
         }
     })
-const ress=result.json()
+const ress=await result.json()
      
     return NextResponse.json({message: "Accident occured",ress},{status:201})
 }
